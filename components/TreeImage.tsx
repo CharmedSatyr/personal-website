fd056b20617b7569c006d5a9e7e78bcaffa270f6
Joseph <electronic@mailbox.org>
2024-05-17 16:30:03 -0700

    Don't load tree until mode is ready

--- components/TreeImage.tsx
+++ components/TreeImage.tsx
@@ -4,9 +4,18 @@ import { useTheme } from "next-themes";
 
 import Image from "components/Image";
 import { Mode } from "constants/mode";
+import { useEffect, useState } from "react";
 
 const TreeImage = () => {
+	const [mounted, setMounted] = useState(false);
 	const { theme } = useTheme();
+
+	useEffect(() => setMounted(true), []);
+
+	if (!mounted) {
+		return null;
+	}
+
 	return (
 		<Image
 			alt="tree"
@@ -14,7 +23,6 @@ const TreeImage = () => {
 			src={`/static/images/${theme === Mode.dark ? "tree" : "tree-contrast"}.webp`}
 			width={50}
 			height={50}
-			priority
 		/>
 	);
 };
