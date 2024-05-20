d9606b1a01ae7cffa8578e7262534808d183cb2b
Joseph <electronic@mailbox.org>
2024-05-20 16:01:22 -0700

    Function and footer refinements

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,12 @@
+import siteMetadata from "@/data/site-metadata";
 import ThemeSwitch from "components/ThemeSwitch";
 
 const Footer = () => {
 	return (
 		<footer className="flex items-center justify-center space-x-2 px-8 py-4">
-			<div>{`Copyright © ${new Date().getFullYear()}`}</div>
+			<div>
+				{`© ${new Date().getFullYear()}`} {siteMetadata.author}
+			</div>
 			<div> • </div>
 			<div>Updated {process.env.lastUpdated}</div>
 			<div> • </div>
--- next.config.js
+++ next.config.js
@@ -2,18 +2,10 @@ const getBuildDate = () => {
 	const date = new Date();
 
 	const year = date.getFullYear().toString();
-	let month = (date.getMonth() + 1).toString();
-	let day = date.getDate().toString();
+	const month = (date.getMonth() + 1).toString();
+	const day = date.getDate().toString();
 
-	if (month.length < 2) {
-		month = `0${month}`;
-	}
-
-	if (day.length < 2) {
-		day = `0${day}`;
-	}
-
-	return [year, month, day].join("-");
+	return [year, month.padStart(2, "0"), day.padStart(2, "0")].join("-");
 };
 
 /** @type {import('next').NextConfig} */
