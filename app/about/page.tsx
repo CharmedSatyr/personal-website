b22f7fb4a9168e8030f9326b5178384dea2daaaf
Joseph <electronic@mailbox.org>
2024-05-29 10:42:05 -0700

    Comment out incomplete page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,10 +8,22 @@ const About = () => {
 		<PageContainer>
 			<h1 className="mb-8">About</h1>
 
+			<p>In Progress...</p>
+			{/*
+			<h2>a</h2>
+
+			<h2>Interests</h2>
 			<p>
 				I love to travel. I've visited 29 countries. I spent 6 weeks in an apartment in Prague last
 				summer, and this summer I have tickets for the Paris Olympics.
 			</p>
+
+			<h2>Uses</h2>
+
+			<h2>Go-To Stack</h2>
+
+			<p></p>
+		*/}
 		</PageContainer>
 	);
 };
