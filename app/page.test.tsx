94a3f1aab21a7348a3ff4db6f953a96faeecf35d
Joseph <electronic@mailbox.org>
2024-06-01 17:47:06 -0700

    Add test

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -1,10 +1,10 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import Page from "./page";
+import Home from "app/page";
 
-describe("Page", () => {
+describe("Home", () => {
 	it("renders a heading", () => {
-		render(<Page />);
+		render(<Home />);
 
 		const heading = screen.getByRole("heading", { level: 1 });
 
@@ -12,10 +12,24 @@ describe("Page", () => {
 	});
 
 	it("renders two paragraphs", () => {
-		render(<Page />);
+		render(<Home />);
 
 		const paragraphs = screen.getAllByRole("paragraph");
 
 		expect(paragraphs.length).toBe(2);
+	});
+
+	it("renders two links", () => {
+		render(<Home />);
+
+		const links = screen.getAllByRole("link");
+
+		expect(links.length).toBe(2);
+	});
+
+	it("should match the last snapshot", () => {
+		const { container } = render(<Home />);
+
+		expect(container).toMatchSnapshot();
 	});
 });
