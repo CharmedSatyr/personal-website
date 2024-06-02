06ea37d8064bc5fcde19581c1270e83a80acf307
Joseph <electronic@mailbox.org>
2024-06-01 18:04:29 -0700

    Add About page tests

---
+++ app/about/page.test.tsx
@@ -0,0 +1,39 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import About from "app/about/page";
+
+describe("About", () => {
+	test.todo("has a header");
+
+	test.todo("has a footer");
+
+	it("renders a heading", () => {
+		render(<About />);
+
+		const heading = screen.getByRole("heading", { level: 1 });
+
+		expect(heading).toBeInTheDocument();
+	});
+
+	it("renders three paragraphs", () => {
+		render(<About />);
+
+		const paragraphs = screen.getAllByRole("paragraph");
+
+		expect(paragraphs.length).toBe(3);
+	});
+
+	it("renders one link", () => {
+		render(<About />);
+
+		const links = screen.getAllByRole("link");
+
+		expect(links.length).toBe(1);
+	});
+
+	it("should match the last snapshot", () => {
+		const { container } = render(<About />);
+
+		expect(container).toMatchSnapshot();
+	});
+});
