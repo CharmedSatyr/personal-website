82c49f4b1ce306cd7f42e677842352afafe5b7a1
Joseph <electronic@mailbox.org>
2024-06-14 14:26:54 -0700

    Restore tsx now page; use tailwind typography

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,16 +1,11 @@
 import type { MDXComponents } from "mdx/types";
 
-import Link from "./components/Link";
-import PageTitle from "./components/PageTitle";
+import Link from "@/components/Link";
+import PageTitle from "@/components/PageTitle";
 
 export const useMDXComponents = (components: MDXComponents): MDXComponents => {
 	return {
 		h1: ({ children }) => <PageTitle>{children}</PageTitle>,
-		ol: ({ children }) => (
-			<ol className="list-decimal leading-relaxed marker:text-accent-600 dark:marker:text-dark-accent-300">
-				{children}
-			</ol>
-		),
 		a: ({ href, children }) => <Link href={href}>{children}</Link>,
 		...components,
 	};
