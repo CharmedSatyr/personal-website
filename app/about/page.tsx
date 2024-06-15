0d6641ecb76c72c27943c1d5469f9367c3d47550
Joseph <electronic@mailbox.org>
2024-06-15 16:06:07 -0700

    Fix photo responsiveness

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -42,9 +42,9 @@ const About = () => {
 					sorcerers or warlocks.
 				</li>
 			</ul>
-			<div className="flex justify-between">
+			<div className="flex flex-col items-center justify-between justify-items-center md:flex-row">
 				<Image
-					className="w-2/6 max-w-sm flex-initial rounded-sm border border-primary-800 dark:border-dark-primary-200"
+					className="m-2 w-5/6 max-w-sm flex-initial rounded-sm border border-primary-800 dark:border-dark-primary-200 md:w-2/6"
 					alt="Looking out a window at Karlštejn Castle, Czech Republic."
 					src="/static/images/joseph.webp"
 					height={450}
@@ -54,7 +54,7 @@ const About = () => {
 					blurDataURL={joseph}
 				/>
 				<Image
-					className="max-w-xs flex-initial rounded-sm border border-primary-800 dark:border-dark-primary-200"
+					className="m-2 max-w-xs flex-initial rounded-sm border border-primary-800 dark:border-dark-primary-200"
 					alt="Standing in front of the Great Wall of China."
 					src="/static/images/great-wall.webp"
 					height={450}
