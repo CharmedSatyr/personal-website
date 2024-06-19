230407fba21884dc117d6bbe6362aaa057b7a7e0
Joseph <electronic@mailbox.org>
2024-06-19 09:14:57 -0700

    Convert games page to mdx

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -6,6 +6,7 @@ import PageTitle from "@/components/PageTitle";
 export const useMDXComponents = (components: MDXComponents): MDXComponents => {
 	return {
 		h1: ({ children }) => <PageTitle>{children}</PageTitle>,
+		h2: ({ children }) => <h2 className="title">{children}</h2>,
 		a: ({ href, children }) => <Link href={href}>{children}</Link>,
 		...components,
 	};
