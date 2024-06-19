6325da8bf748d7f28a32c0a4b9cb6e0430a74bfc
Joseph <electronic@mailbox.org>
2024-06-19 11:02:06 -0700

    Add test todos

---
+++ app/games/recently-played.test.tsx
@@ -0,0 +1,13 @@
+describe("recently-played", () => {
+	test.todo("getRecentlyPlayed success");
+	test.todo("getRecentlyPlayed failure");
+
+	test.todo("getGameMetadata success");
+	test.todo("getGameMetadata failure");
+
+	test.todo("getRecentGameMetadata success");
+	test.todo("getRecentGameMetadata failure");
+
+	test.todo("render success");
+	test.todo("render failure");
+});
