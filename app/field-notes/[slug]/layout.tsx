946254985d42d5d249a8680a7637b7b82214a0d1
Joseph <electronic@mailbox.org>
2024-06-25 20:14:48 -0700

    Manually capitalize titles

--- app/field-notes/[slug]/layout.tsx
+++ app/field-notes/[slug]/layout.tsx
@@ -1,5 +1,5 @@
 const Layout = ({ children }) => (
-	<article className="prose-headings:title prose-img:image prose-figcaption:caption prose-p:text prose-a:hyperlink prose-ol:list prose-ol:text prose mb-20 dark:prose-invert prose-h1:capitalize prose-ol:list-decimal prose-img:mb-0">
+	<article className="prose-headings:title prose-img:image prose-figcaption:caption prose-p:text prose-a:hyperlink prose-ol:list prose-ol:text prose mb-20 dark:prose-invert prose-ol:list-decimal prose-img:mb-0">
 		{children}
 	</article>
 );
