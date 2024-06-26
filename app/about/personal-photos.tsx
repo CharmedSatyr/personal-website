67aebb588da9c40be570ebad0b36ac44d7a7aabe
Joseph <electronic@mailbox.org>
2024-06-25 19:13:10 -0700

    Add learning to code blog. Captions. Styling tweaks

--- app/about/personal-photos.tsx
+++ app/about/personal-photos.tsx
@@ -6,26 +6,36 @@ import Image from "@/components/image";
 const PersonalPhotos = () => {
 	return (
 		<div className="mb-6 flex flex-col items-center justify-between justify-items-center md:flex-row">
-			<Image
-				className="m-2 w-3/6 max-w-sm flex-initial rounded-sm border border-primary-800 dark:border-dark-primary-200 md:w-2/6"
-				alt="Looking out a window at Karlštejn Castle, Czech Republic."
-				src="/static/images/joseph.webp"
-				height={450}
-				width={600}
-				priority
-				placeholder="blur"
-				blurDataURL={joseph}
-			/>
-			<Image
-				className="m-2 max-w-xs flex-initial rounded-sm border border-primary-800 dark:border-dark-primary-200"
-				alt="Standing in front of the Great Wall of China."
-				src="/static/images/great-wall.webp"
-				height={450}
-				width={600}
-				priority
-				placeholder="blur"
-				blurDataURL={greatWall}
-			/>
+			<figure className="m-2 w-3/6 max-w-sm flex-initial md:w-2/6">
+				<Image
+					className="image"
+					alt="Looking out a window at Karlštejn Castle, Czech Republic."
+					src="/static/images/joseph.webp"
+					height={450}
+					width={600}
+					priority
+					placeholder="blur"
+					blurDataURL={joseph}
+				/>
+				<figcaption className="caption">
+					Karlštejn Castle, Czech Republic
+				</figcaption>
+			</figure>
+			<figure className="m-2 max-w-xs flex-initial">
+				<Image
+					className="image"
+					alt="Standing in front of the Great Wall of China."
+					src="/static/images/great-wall.webp"
+					height={450}
+					width={600}
+					priority
+					placeholder="blur"
+					blurDataURL={greatWall}
+				/>
+				<figcaption className="caption">
+					The Great Wall near Beijing, China
+				</figcaption>
+			</figure>
 		</div>
 	);
 };
