1c0dd76e85a6dfa6d9b53b39283888d5ee8bc06b
Joseph <electronic@mailbox.org>
2024-06-26 19:59:13 -0700

    Remove mounting check since it's being loaded dynamically in layout

--- components/responsive-footer.tsx
+++ components/responsive-footer.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import useBreakpoints from "hooks/useBreakpoints";
 import { useWindowSize } from "hooks/useWindowSize";
 
@@ -8,15 +7,10 @@ import CollapsibleFooterNav from "@/components/collapsible-footer-nav";
 import StaticFooter from "@/components/static-footer";
 
 const ResponsiveFooter = () => {
-	const [isClient, setIsClient] = useState<boolean>(false);
 	const { md } = useBreakpoints();
 	const windowSize = useWindowSize();
 
-	useEffect(() => {
-		setIsClient(true);
-	}, []);
-
-	if (isClient && windowSize < md) {
+	if (windowSize < md) {
 		return <CollapsibleFooterNav />;
 	}
 
