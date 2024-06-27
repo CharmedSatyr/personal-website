9b139834f7cb864ae50aea0e1468493f1c7518f3
Joseph <electronic@mailbox.org>
2024-06-26 20:10:35 -0700

    Fix hidden nav breakpoint

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
 		<header className="fixed left-0 right-0 top-0 flex w-screen items-center justify-between bg-primary-50 px-8 py-4 opacity-75 drop-shadow dark:bg-primary-950">
 			<Link href="/">{siteMetadata.headerTitle}</Link>
 
-			<div className="flex hidden items-center space-x-2 sm:inline">
+			<div className="flex hidden items-center space-x-2 md:inline">
 				<NavLinks />
 			</div>
 		</header>
