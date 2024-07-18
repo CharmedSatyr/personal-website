774466736ee0aa4ba8542c79e5d1a38009a20c3c
Joseph Wolfe <joseph@WLF.to>
2024-07-18 11:24:45 -0700

    Add a couple more specific tests for field-notes

--- app/field-notes/page.test.tsx
+++ app/field-notes/page.test.tsx
@@ -1,10 +1,22 @@
 import FieldNotes from "@/app/field-notes/page";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 describe("Field Notes", () => {
 	it("should not deviate unexpectedly from the last snapshot", () => {
 		const { container } = render(<FieldNotes />);
 
 		expect(container).toMatchSnapshot();
+	});
+
+	it("renders a heading", () => {
+		render(<FieldNotes />);
+
+		expect(screen.getByRole("heading", { level: 1 })).toBeInTheDocument();
+	});
+
+	it("renders a link", () => {
+		render(<FieldNotes />);
+
+		expect(screen.getAllByRole("link")).toHaveLength(1);
 	});
 });
