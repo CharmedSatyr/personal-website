12a62374885ceb0455f619404919d7a0088d6dcd
Joseph Wolfe <joseph@WLF.to>
2024-08-22 17:30:38 -0700

    Refine header border on small devices

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import siteMetadata from "@/data/site-metadata";
 
 const Header = () => {
 	return (
-		<header className="fixed flex w-full items-center justify-between rounded border border-primary-400 bg-primary-50 px-8 py-4 drop-shadow-lg dark:border-primary-800 dark:bg-primary-950/90 sm:mt-4 sm:w-11/12 md:w-11/12 lg:w-9/12 2xl:w-7/12">
+		<header className="fixed flex w-full items-center justify-between border-b border-primary-400 bg-primary-50 px-8 py-4 drop-shadow-lg dark:border-primary-800 dark:bg-primary-950/90 sm:mt-4 sm:w-11/12 sm:rounded sm:border md:w-11/12 lg:w-9/12 2xl:w-7/12">
 			<Link href="/">{siteMetadata.headerTitle}</Link>
 
 			<div className="flex hidden items-center space-x-2 md:inline">
