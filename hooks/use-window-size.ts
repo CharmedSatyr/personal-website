f042e00ac0e4f84a83680223184a02be6ee5bbe8
Joseph Wolfe <joseph@WLF.to>
2024-12-10 04:08:54 -0800

    Remove mounted logic

--- hooks/use-window-size.ts
+++ hooks/use-window-size.ts
@@ -3,32 +3,19 @@
 import { useEffect, useState } from "react";
 
 const useWindowSize = (): number => {
-	const [mounted, setMounted] = useState<boolean>(false);
 	const [windowSize, setWindowSize] = useState<number>(0);
 
 	useEffect(() => {
-		setMounted(true);
-	}, []);
-
-	useEffect(() => {
-		if (!mounted) {
-			return;
-		}
-
 		setWindowSize(window.innerWidth);
-	}, [mounted, windowSize]);
+	}, [windowSize]);
 
 	useEffect(() => {
-		if (!mounted) {
-			return;
-		}
-
 		const updateWindowSize = () => setWindowSize(window.innerWidth);
 
 		window.addEventListener("resize", updateWindowSize);
 
 		return () => window.removeEventListener("resize", updateWindowSize);
-	}, [mounted]);
+	}, []);
 
 	return windowSize;
 };
